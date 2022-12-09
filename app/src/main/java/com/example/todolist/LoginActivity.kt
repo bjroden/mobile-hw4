a1ceb3b067476e848ab49a81a0ddfd95ff667902
bjroden <bjroden@uark.edu>
2022-12-08 21:10:20 -0600

    Per user items

--- app/src/main/java/com/example/todolist/LoginActivity.kt
+++ app/src/main/java/com/example/todolist/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.example.todolist
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -29,6 +31,7 @@ class LoginActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     Log.d("MainActivity", "sign up successful: ${auth.currentUser?.uid}")
+                    launchMainActivity()
                 }
                 else {
                     Log.d("MainActivity", task.exception.toString())
@@ -43,10 +46,18 @@ class LoginActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     Log.d("MainActivity", "sign in successful: ${auth.currentUser?.uid}")
+                    launchMainActivity()
                 }
                 else {
                     Log.d("MainActivity", task.exception.toString())
                 }
             }
+    }
+
+    private val mainActivityLauncher = registerForActivityResult(StartActivityForResult()) { }
+
+    private fun launchMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        mainActivityLauncher.launch(intent)
     }
 }
--- app/src/main/java/com/example/todolist/ToDoListViewModel.kt
+++ app/src/main/java/com/example/todolist/ToDoListViewModel.kt
@@ -1,30 +1,51 @@
 package com.example.todolist
 
+import android.util.Log
 import androidx.lifecycle.*
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 
 class ToDoListViewModel(private val repository: ToDoItemRepository): ViewModel() {
 
-    private val firebaseDb = Firebase.firestore.collection("items-test")
+    val auth = Firebase.auth
+    var uid = auth.currentUser?.uid
+
+    private val firebaseDb = Firebase.firestore.collection("users")
+
+    private fun userItems() = firebaseDb.document(uid.toString()).collection("items")
+
+    init {
+        Log.d("MainActivity", "$uid")
+        uid?.let { user ->
+            // TODO: see if this is the correct way to initialize user collections
+            firebaseDb.document(user).get().addOnCompleteListener { result ->
+                if (result.result.data == null) {
+                    firebaseDb.document(user).set(hashMapOf(
+                        "items" to emptyList<ToDoItem>()
+                    ))
+                }
+            }
+        }
+    }
 
     // Livedata used by activities to see item status
     val allToDoItems: LiveData<List<ToDoItem>> = repository.allToDoItems.asLiveData()
 
     fun insert(todoItem: ToDoItem) = viewModelScope.launch {
         val id = repository.insert(todoItem)
-        firebaseDb.document(id.toString()).set(todoItem.toMap())
+        userItems().document(id.toString()).set(todoItem.toMap())
     }
 
     fun updateItem(todoItem: ToDoItem) = viewModelScope.launch {
         repository.updateItem(todoItem)
-        todoItem.id?.let { firebaseDb.document(it.toString()).set(todoItem.toMap()) }
+        todoItem.id?.let { userItems().document(it.toString()).set(todoItem.toMap()) }
     }
 
     fun deleteItem(todoItem: ToDoItem) = viewModelScope.launch {
         repository.deleteItem(todoItem)
-        todoItem.id?.let { firebaseDb.document(it.toString()).delete() }
+        todoItem.id?.let { userItems().document(it.toString()).delete() }
     }
 
     class ToDoListViewModelFactory(private val repository: ToDoItemRepository) : ViewModelProvider.Factory{
